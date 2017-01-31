d92677c27bbd7a3b92d62e3044e9d206346f638d
Mi8ha5 <mi8ha5@yandex.ru>
2017-01-31 21:40:08 +0300

    add
    add

---
+++ app.js
@@ -0,0 +1,6 @@
+var num = document.getElementById("number1");
+//function myFunc(){
+//num.innerHTML = "987654321";
+//}
+var text = num.textContent;
+//document.getElementById("total").innerHTML = text;
