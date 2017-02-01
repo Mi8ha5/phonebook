0d9fb0b0697ebfd20b65dc5e4eaae935d266091f
Mi8ha5 <tetarenko@informatica.ru>
2017-02-01 12:25:44 +0300

    изменения

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ var express = require ('express');
 var app = express();
 var router = express.Router();
 var path = require('path');
+var fs = require('fs');
 
-app.use('/static', express.static(path.join(process.cwd(), 'static')));
+// app.use('/static', express.static(path.join(process.cwd(), 'static')));
 
 router.get('/', function(req,res){
   res.sendFile(path.join(process.cwd(), 'static/index.html'));
 });
+router.get('/static/:filename', function(req,res){
+  // res.sendFile(path.join(process.cwd(), ''))
+  res.send(req.params.filename);
+  fs.readdir(path.join(process.cwd(), 'static'), function(err, files){
+    if (err) return res.sendStatus(500);
+    console.log("Список файлов:", files);
+  });
+})
 app.use(router);
 app.listen(8080);
