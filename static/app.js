6e43f9dec854bd08ca7842b0c27b8281110a6bd1
Mi8ha5 <mi8ha5@yandex.ru>
2017-02-04 22:26:44 +0300

    изменения в XMLHttp

--- static/app.js
+++ static/app.js
@@ -1,7 +1,7 @@
 document.getElementById("number1").innerHTML = "HELLO";
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.open('GET', '/static/phonebook.json', false);
-xmlhttp.send(null);
-if(xmlhttp.status == 200) {
+xmlhttp.send();
+ {
   document.getElementById("output").innerHTML = xmlhttp.responseText;
 };
