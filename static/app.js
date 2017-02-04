091a3b4c21fa2ce118abbfb0b4ca309ef85c3416
Mi8ha5 <mi8ha5@yandex.ru>
2017-02-04 22:15:49 +0300

    показывает содержимое .json на экране при помощи XMLHttp

--- static/app.js
+++ static/app.js
@@ -1,1 +1,7 @@
 document.getElementById("number1").innerHTML = "HELLO";
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open('GET', '/static/phonebook.json', false);
+xmlhttp.send(null);
+if(xmlhttp.status == 200) {
+  document.getElementById("output").innerHTML = xmlhttp.responseText;
+};
