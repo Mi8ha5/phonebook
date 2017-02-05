40245493798583d346cea5de9c501b8fb511a3c6
Mi8ha5 <mi8ha5@yandex.ru>
2017-02-05 23:42:41 +0300

    добавлен вывод имен

--- static/app1.js
+++ static/app1.js
@@ -1,3 +1,10 @@
 $("#number1").html("ПРОВЕРКА РАБОТЫ");
 $("#output").load("phonebook.json");
 $("#number1").append(" и снова проверка");
+$.getJSON( "phonebook.json", function( data ) {
+    $.each( data, function( index, value ) {
+      var items = [];
+    items.push(value["id"]+"➤"+value["name"]);
+    $("<div/>" , {html:items.join('')}).appendTo("body");
+ });
+ });
