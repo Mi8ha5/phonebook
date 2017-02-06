e9699739e3ef4b92c7b541b8e9597e13e5dddf4c
Mi8ha5 <tetarenko@informatica.ru>
2017-02-06 13:19:30 +0300

    new

--- static/app1.js
+++ static/app1.js
@@ -1,10 +1,19 @@
 $("#number1").html("ПРОВЕРКА РАБОТЫ");
-$("#output").load("phonebook.json");
+// $("#output").load("phonebook.json");
 $("#number1").append(" и снова проверка");
-$.getJSON( "phonebook.json", function( data ) {
-    $.each( data, function( index, value ) {
-      var items = [];
-    items.push(value["id"]+"➤"+value["name"]);
-    $("<div/>" , {html:items.join('')}).appendTo("body");
- });
+$.getJSON( "phonebook.json", function(data) {
+  data.forEach(function (item) {
+    $("<div/>" , {id: item.id, html:item.id+"➤"+item.name}).appendTo("body");
+    $("<input/>", {id:item.id+item.name, type:'text', val:item.phone}).appendTo('#'+item.id);
+    //$("#"+item.id+item.name).val(item.phone);
+    $("<button></button>", {id:"b"+item.id, text:'СОХРАНИТЬ'}).appendTo('#'+item.id);
+    //$('#'+'b'+item.id).text('СОХРАНИТЬ');
+    $('#'+'b'+item.id).click(function (){
+      var phbook = {
+        "id" : item.id;
+        "phone" : $('#'+item.id+item.name).val();
+      };
+      alert($('#'+item.id+item.name).val());
+    });
+  });
  });
